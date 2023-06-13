39fe4d5c0bea63aaef7164b53c957d02df15287a
Aristotle <a.debride.2016@gmail.com>
2023-06-13 16:46:55 -0400

    Meat and potatoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Portfolio';
 }
+
+window.onscroll = function () {
+  var top = window.scrollY;
+  if (top >= 100) {
+    header?.classList.add('navbarDark');
+  }
+  else {
+    header?.classList.remove('navbarDark');
+  }
+
+}
+const header = document.querySelector('.navbar');
