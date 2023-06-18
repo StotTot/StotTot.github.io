3922e748dfb7290a0e9a093e0707e78940fc4a43
Aristotle <a.debride.2016@gmail.com>
2023-06-18 14:56:21 -0400

    big changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
   title = 'Portfolio';
 }
-
-window.onscroll = function () {
-  const top = window.scrollY;
-  if (top >= 100) {
-    header?.classList.add('navbarDark');
-  }
-  else {
-    header?.classList.remove('navbarDark');
-  }
-
-}
-const header = document.querySelector('.navbar');
