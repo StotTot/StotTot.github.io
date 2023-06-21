9ee7e3a1c195ef00e3e97dc785c4795055019bcc
Aristotle <a.debride.2016@gmail.com>
2023-06-21 19:41:19 -0400

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,9 +15,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Portfolio'`, () => {
+  it(`should have as title 'Aristotle'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Portfolio');
+    expect(app.title).toEqual('Aristotle');
   });
 });
